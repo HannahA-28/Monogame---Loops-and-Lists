23c2809bf4586214b0285c16eab4944ce03be1bd
HannahA-28 <339729345@2260I-L29-031>
2026-04-07 13:18:46 -0400

    Loading planets

--- Monogame - Loops and Lists/Game1.cs
+++ Monogame - Loops and Lists/Game1.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
 
 namespace Monogame___Loops_and_Lists
 {
@@ -9,6 +11,7 @@ namespace Monogame___Loops_and_Lists
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -16,9 +19,29 @@ namespace Monogame___Loops_and_Lists
             IsMouseVisible = true;
         }
 
+        Random generator;
+        Rectangle window;
+        Texture2D spaceBackgroundTexture;
+        List<Texture2D> textures;
+        List<Rectangle> planetRects;
+
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+
+            window = new Rectangle(0, 0, _graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferWidth);
+            generator = new Random();
+            textures = new List<Texture2D>();
+            planetRects = new List<Rectangle>();
+
+            for (int i = 0; i < 30;  i++)
+            {
+                planetRects.Add
+                    (
+                        new Rectangle(generator.Next(window.Width - 25), 
+                        generator.Next(window.Height - 25), 25, 25)
+                    );
+            }
 
             base.Initialize();
         }
@@ -28,6 +51,11 @@ namespace Monogame___Loops_and_Lists
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+
+            spaceBackgroundTexture = Content.Load<Texture2D>("Images/space_background[1]");
+            for (int i = 1; i <= 13; i++)
+                textures.Add(Content.Load<Texture2D>("Images/16-bit-planet[1]" + i));
+
         }
 
         protected override void Update(GameTime gameTime)
@@ -45,6 +73,14 @@ namespace Monogame___Loops_and_Lists
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+
+            _spriteBatch.Begin();
+
+            _spriteBatch.Draw(spaceBackgroundTexture, window, Color.White);
+            for (int i = 0; i < planetRects.Count; i++)
+                _spriteBatch.Draw(textures[0], planetRects[i], Color.White);
+
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
